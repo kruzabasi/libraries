4b4e7b31381ca53701b8ec8de0e69802c8b7850b
kruzabasi <kruzabasi@gmail.com>
2020-05-10 20:30:59 +0100

    nodemon configured

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ const morgan = require("morgan");
 const path = require("path");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan("tiny"));
 
 app.use(express.static(path.join(__dirname, "public/")));
-//app.use js, css and jquery from node modules dir
+// app.use js, css and jquery from node modules dir
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/index.html"));
 });
 
-app.listen(3000, function() {
-  debug(`listening on port ${chalk.green(3000)}`);
+app.listen(port, () => {
+  debug(`listening at port ${chalk.green(port)}`);
 });
