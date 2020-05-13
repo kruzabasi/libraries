758931cd9e4120c27028a8f485b2ef78829e15ad
kruzabasi <kruzabasi@gmail.com>
2020-05-14 00:10:20 +0100

    single book query defined

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,91 +1,56 @@
 const express = require("express");
+const { MongoClient, ObjectID } = require("mongodb");
+const debug = require("debug")("app:bookRoutes");
 const bookRouter = express.Router();
 
 function router(nav) {
-  const books = [
-    {
-      title: "Richest Man in Babylon",
-      genre: "Finance",
-      author: "Alfred Hunter",
-      read: false
-    },
-    {
-      title: "His Dark Materials",
-      genre: "Fiction",
-      author: "Rick Banning",
-      read: false
-    },
-    {
-      title: "Half of a Yellow Sun",
-      genre: "Folk",
-      author: "Chiwetele Ejiofor",
-      read: false
-    },
-    {
-      title: "War Crimes",
-      genre: "Docmentary",
-      author: "Tokoyami Obanai",
-      read: false
-    },
-    {
-      title: "Naked Forex",
-      genre: "Finance",
-      author: "Don Corleon",
-      read: false
-    },
-    {
-      title: "Evil Forest",
-      genre: "Fiction",
-      author: "Ada Obubu",
-      read: false
-    },
-    {
-      title: "War and Peace",
-      genre: "Folk",
-      author: "Lev Nikolayevich Tolstoy",
-      read: false
-    },
-    {
-      title: "Les Misreables",
-      genre: "Fantasy",
-      author: "Victor Hugo",
-      read: false
-    },
-    {
-      title: "The Dark World",
-      genre: "Fiction",
-      author: "Henry Kutner",
-      read: false
-    },
-    {
-      title: "The Time Machine",
-      genre: "Fiction",
-      author: "H. G. Wells",
-      read: false
-    },
-    {
-      title: "Wind in The Willows",
-      genre: "Fantasy",
-      author: "Kenneth Grahame",
-      read: false
-    },
-    { title: "African Giant", genre: "Folk", author: "Burna Boy", read: false }
-  ];
-
   bookRouter.route("/").get((req, res) => {
-    res.render("books", {
-      nav,
-      title: "Library",
-      books
-    });
+    const url = "mongodb://localhost:27017";
+    const dbName = "libraryApp";
+
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug("client connected successfully");
+        const db = client.db(dbName);
+        const col = await db.collection("books");
+
+        const books = await col.find().toArray();
+
+        res.render("books", {
+          nav,
+          title: "Library",
+          books
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+      client.close();
+    })();
   });
   bookRouter.route("/:id").get((req, res) => {
     const { id } = req.params;
-    res.render("bookView", {
-      nav,
-      title: "Library",
-      book: books[id]
-    });
+    const url = "mongodb://localhost:27017";
+    const dbName = "libraryApp";
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug("client connected successfully");
+        const db = client.db(dbName);
+        const col = await db.collection("books");
+        const book = await col.findOne({ _id: new ObjectID(id) });
+        debug(book);
+        res.render("bookView", {
+          nav,
+          title: "Library",
+          book
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+    })();
   });
   return bookRouter;
 }
