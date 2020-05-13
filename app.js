e5c2a27811e534df884f7a4abf5ed5ee037dbaa6
kruzabasi <kruzabasi@gmail.com>
2020-05-13 11:08:18 +0100

    routes broken into bits. bookview added

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
-const bookRouter = express.Router();
 
 app.use(morgan("tiny"));
 app.use(express.static(path.join(__dirname, "/public/")));
@@ -25,86 +24,9 @@ app.use(
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-const books = [
-  {
-    title: "Richest Man in Babylon",
-    genre: "Finance",
-    author: "Alfred Hunter",
-    read: false
-  },
-  {
-    title: "His Dark Materials",
-    genre: "Fiction",
-    author: "Rick Banning",
-    read: false
-  },
-  {
-    title: "Half of a Yellow Sun",
-    genre: "Folk",
-    author: "Chiwetele Ejiofor",
-    read: false
-  },
-  {
-    title: "War Crimes",
-    genre: "Docmentary",
-    author: "Tokoyami Obanai",
-    read: false
-  },
-  {
-    title: "Naked Forex",
-    genre: "Finance",
-    author: "Don Corleon",
-    read: false
-  },
-  { title: "Evil Forest", genre: "Fiction", author: "Ada Obubu", read: false },
-  {
-    title: "War and Peace",
-    genre: "Folk",
-    author: "Lev Nikolayevich Tolstoy",
-    read: false
-  },
-  {
-    title: "Les Misreables",
-    genre: "Fantasy",
-    author: "Victor Hugo",
-    read: false
-  },
-  {
-    title: "The Dark World",
-    genre: "Fiction",
-    author: "Henry Kutner",
-    read: false
-  },
-  {
-    title: "The Time Machine",
-    genre: "Fiction",
-    author: "H. G. Wells",
-    read: false
-  },
-  {
-    title: "Wind in The Willows",
-    genre: "Fantasy",
-    author: "Kenneth Grahame",
-    read: false
-  },
-  { title: "African Giant", genre: "Folk", author: "Burna Boy", read: false }
-];
-
-bookRouter.route("/").get((req, res) => {
-  res.render("books", {
-    nav: [
-      { link: "/books", title: "Books" },
-      { link: "/authors", title: "Authors" }
-    ],
-    title: "Library",
-    books
-  });
-});
-bookRouter.route("/single").get((req, res) => {
-  res.send("hello single book");
-});
-
+const bookRouter = require("./src/routes/bookRoutes");
 app.use("/books", bookRouter);
+
 app.get("/", (req, res) => {
   res.render("index", {
     nav: [
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const bookRouter = express.Router();
+
+const books = [
+  {
+    title: "Richest Man in Babylon",
+    genre: "Finance",
+    author: "Alfred Hunter",
+    read: false
+  },
+  {
+    title: "His Dark Materials",
+    genre: "Fiction",
+    author: "Rick Banning",
+    read: false
+  },
+  {
+    title: "Half of a Yellow Sun",
+    genre: "Folk",
+    author: "Chiwetele Ejiofor",
+    read: false
+  },
+  {
+    title: "War Crimes",
+    genre: "Docmentary",
+    author: "Tokoyami Obanai",
+    read: false
+  },
+  {
+    title: "Naked Forex",
+    genre: "Finance",
+    author: "Don Corleon",
+    read: false
+  },
+  { title: "Evil Forest", genre: "Fiction", author: "Ada Obubu", read: false },
+  {
+    title: "War and Peace",
+    genre: "Folk",
+    author: "Lev Nikolayevich Tolstoy",
+    read: false
+  },
+  {
+    title: "Les Misreables",
+    genre: "Fantasy",
+    author: "Victor Hugo",
+    read: false
+  },
+  {
+    title: "The Dark World",
+    genre: "Fiction",
+    author: "Henry Kutner",
+    read: false
+  },
+  {
+    title: "The Time Machine",
+    genre: "Fiction",
+    author: "H. G. Wells",
+    read: false
+  },
+  {
+    title: "Wind in The Willows",
+    genre: "Fantasy",
+    author: "Kenneth Grahame",
+    read: false
+  },
+  { title: "African Giant", genre: "Folk", author: "Burna Boy", read: false }
+];
+
+bookRouter.route("/").get((req, res) => {
+  res.render("books", {
+    nav: [
+      { link: "/books", title: "Books" },
+      { link: "/authors", title: "Authors" }
+    ],
+    title: "Library",
+    books
+  });
+});
+bookRouter.route("/:id").get((req, res) => {
+  const { id } = req.params;
+  res.render("bookView", {
+    nav: [
+      { link: "/books", title: "Books" },
+      { link: "/authors", title: "Authors" }
+    ],
+    title: "Library",
+    book: books[id]
+  });
+});
+
+module.exports = bookRouter;
