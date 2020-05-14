cf7021067cd1bea401187216578cee6343c87a4b
kruzabasi <kruzabasi@gmail.com>
2020-05-14 16:04:10 +0100

    local strategy implemented

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,7 +6,13 @@ const authRouter = express.Router();
 function router() {
   authRouter.route("/signUp").post((req, res) => {
     debug(req.body);
-    res.json(req.body);
+    //create user
+    req.login(req.body, () => {
+      res.redirect("/auth/profile");
+    });
+  });
+  authRouter.route("/profile").get((req, res) => {
+    res.json(req.user);
   });
   return authRouter;
 }
